c8c54339f8bd0dc4281eff1178abff61affd365a
Hamilton Chapman <hamchapman@gmail.com>
2015-08-18 16:24:26 +0500

    Initial Commit

---
+++ SwiftGQL/SwiftGQL.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGQL.h
+//  SwiftGQL
+//
+//  Created by Hamilton Chapman on 18/08/2015.
+//  Copyright © 2015 hc.gg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftGQL.
+FOUNDATION_EXPORT double SwiftGQLVersionNumber;
+
+//! Project version string for SwiftGQL.
+FOUNDATION_EXPORT const unsigned char SwiftGQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGQL/PublicHeader.h>
+
+
